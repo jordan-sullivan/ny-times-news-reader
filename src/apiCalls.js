42c95ef99cd2edbc8f84d919a577796d88ac15d7
Jordan Sullivan <95270427+jordan-sullivan@users.noreply.github.com>
2022-09-20 15:51:56 -0600

    add section in fetch call

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,7 +1,6 @@
 //API Key: g0iIp2fZh3Hy7ztGZuCLlgPtuR8ysniZ
 
 const fetchTopStories = () => {
-// return fetch(`https://api.nytimes.com/svc/topstories/v2/${section}.json?api-key=g0iIp2fZh3Hy7ztGZuCLlgPtuR8ysniZ`)
 return fetch("https://api.nytimes.com/svc/topstories/v2/arts.json?api-key=g0iIp2fZh3Hy7ztGZuCLlgPtuR8ysniZ")
     .then((response) => {
     if (response.ok) {
