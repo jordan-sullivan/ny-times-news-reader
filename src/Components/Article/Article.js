801e564f6e57d2b346d0484be90ce6590b0f63b2
Jordan Sullivan <95270427+jordan-sullivan@users.noreply.github.com>
2022-09-22 22:23:23 -0600

    Center images and text  on main page

--- src/Components/Article/Article.js
+++ src/Components/Article/Article.js
@@ -1,16 +1,19 @@
 import React from "react";
+import "../Article/article.css"
 
 
 const Article = ({ title, abstract, multimedia, byline }) => {
     let images = multimedia ? <img src={multimedia[1].url} alt={multimedia[1].caption} className='article-img'/> : null
    
-   return (
-        <article className='article-card'>
-            <h2>{title}</h2>
-            {images}
-            <p>{abstract}</p>
-            <p>{byline}</p>
-        </article>
+    return (
+        <div className="holder">
+            <article className="art-holder">
+                <h2 className='article-card-text'>{title}</h2>
+           {images}
+                <p className='article-card-text'>{abstract}</p>
+                <p className="byline">{byline}</p>
+            </article>
+        </div>
     )
 }
 
--- src/Components/ArticleContainer/ArticleContainer.js
+++ src/Components/ArticleContainer/ArticleContainer.js
@@ -1,5 +1,5 @@
 import React from "react";
-import './articleContainer.css'
+import '../ArticleContainer/articleContainer.css'
 import Article from "../Article/Article";
 import { Link } from "react-router-dom";
 
@@ -8,7 +8,8 @@ const ArticleContainer = ({ articles }) => {
     console.log(articles)
     const articleCards = articles.map((article, index) => {
         return (
-            <Link to={`/article/${index}`} style={{textDecoration: 'none', color: 'black', fontFamily: 'Lora'}} className='article-card' key={index}>
+            <Link to={`/article/${index}`} style={{textDecoration: 'none', color: 'black', fontFamily: 'Lora'}} 
+            className='article-card' key={index}>
                 <Article
                     title={article.title}
                     abstract={article.abstract}
